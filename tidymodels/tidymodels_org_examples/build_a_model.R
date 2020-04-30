7d68da791e29fc6340c092a0d1368c3ba95f7998
Richard Kmieciak <richard.kmieciak@avast.com>
2020-04-30 08:48:56 -0700

    import urchins data

--- tidymodels/tidymodels_org_examples/build_a_model.R
+++ tidymodels/tidymodels_org_examples/build_a_model.R
@@ -1,1 +1,7 @@
+library(tidyverse)
 library(tidymodels)
+
+urchins <-
+    read_csv("https://tidymodels.org/start/models/urchins.csv") %>% 
+    setNames(c("food_regime", "initial_volume", "width")) %>% 
+    mutate(food_regime = factor(food_regime, levels = c("Initial", "Low", "High")))
