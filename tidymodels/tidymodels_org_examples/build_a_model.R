c1a8f3fe46af0709c6c3bcb5fcd6585375db1aeb
Music <music@Musics-MacBook-Pro.local>
2020-05-02 21:59:14 -0700

    cleanup

--- tidymodels/tidymodels_org_examples/build_a_model.R
+++ tidymodels/tidymodels_org_examples/build_a_model.R
@@ -1,7 +1,6 @@
 library(tidyverse)
 library(tidymodels)
 
-urchins <-
-    read_csv("https://tidymodels.org/start/models/urchins.csv") %>% 
+urchins <- read_csv("https://tidymodels.org/start/models/urchins.csv") %>% 
     setNames(c("food_regime", "initial_volume", "width")) %>% 
     mutate(food_regime = factor(food_regime, levels = c("Initial", "Low", "High")))
